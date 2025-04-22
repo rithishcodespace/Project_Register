3c9468c8aa10dfcd67ac814098ffd0659493dc16
Mathan-2007 <mathan007.2007@gmail.com>
2025-04-22 08:57:37 +0530

    add

--- frontend/src/Student_Navbar.jsx
+++ frontend/src/Student_Navbar.jsx
@@ -4,15 +4,10 @@ import logout from "./assets/logout.webp"
 
 function Student_navbar() {
   return (
-    <div style={{width:"15%",height:"100vh",backgroundColor:"white",textAlign:"center"}}>
+    <div style={{width:"18%",height:"100vh",backgroundColor:"white",textAlign:"center"}}>
       <img src={college_img} style={{width:"50%",marginTop:"10%", backgroundColor:"white",zIndex:1}} alt="" />
-      <div><img src={logout} style={{width:"15%"}} alt="" /><p>Dashboard</p></div>
-      <div><img src={logout} style={{width:"15%"}} alt="" /><p>Dashboard</p></div>
-      <div><img src={logout} style={{width:"15%"}} alt="" /><p>Dashboard</p></div>
-      <div><img src={logout} style={{width:"15%"}} alt="" /><p>Dashboard</p></div>
-      <div><img src={logout} style={{width:"15%"}} alt="" /><p>Dashboard</p></div>
-
-      <div style={{backgroundColor:"white",marginBottom:"10px"}}><img src={logout} alt="" style={{width:"15%",backgroundColor:"white",color:"rgb(158, 67, 255);"}} /></div>
+          
+      
     </div>
   )
 }
