5c5153a243dff56e6a904d40268f0d3edc88e323
PRAKASH-SK <prakashpv.cs24@bitsathy.ac.in>
2025-04-23 15:53:57 +0530

    update

--- frontend/src/components/Students/Student.jsx
+++ frontend/src/components/Students/Student.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import Navbar from './Student_Navbar'
+import React, { useState } from 'react'
 import Header from '../../Header'
 import Student_navbar from './Student_Navbar'
 import { Outlet } from 'react-router-dom'
 
 function Student() {
+   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  
+    const toggleSidebar = () => {
+      setIsSidebarOpen(!isSidebarOpen);
+    };
+
   return (
     <div className="flex min-h-screen bg-white">
-    <Student_navbar />
-    <div className="flex-1">
-      <Header />
-      <div className="p-4">
-        <Outlet />
-      </div>
+     <Student_navbar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+        <div className={`transition-all duration-500 flex-1 overflow-y-auto h-screen ${isSidebarOpen ? 'ml-64' : 'ml-24'}`}>
+          <Header />
+            <div className="p-4">
+              <Outlet />
+            </div>
+        </div>
     </div>
-  </div>
   )
 }
 
--- frontend/src/components/Teacher/Teacher.jsx
+++ frontend/src/components/Teacher/Teacher.jsx
@@ -13,18 +13,13 @@ function Teacher() {
   return (
     <div className="flex min-h-screen bg-white">
       <Teacher_navbar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-      <div
-  className={`transition-all duration-500 flex-1 overflow-y-auto h-screen ${
-    isSidebarOpen ? 'ml-64' : 'ml-24'
-  }`}
->
-  <Header />
-  <div className="p-4">
-    <Outlet />
-  </div>
-</div>
-
-      </div>
+        <div className={`transition-all duration-500 flex-1 overflow-y-auto h-screen ${isSidebarOpen ? 'ml-64' : 'ml-24'}`}>
+          <Header />
+            <div className="p-4">
+              <Outlet />
+            </div>
+        </div>
+    </div>
     
   );
 }
