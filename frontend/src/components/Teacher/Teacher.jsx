0b9607f1f54643bc1ffecf52acd7b7591de53c52
PRAKASH-SK <prakashpv.cs24@bitsathy.ac.in>
2025-04-23 08:50:35 +0530

    update

--- frontend/src/components/Teacher/Teacher.jsx
+++ frontend/src/components/Teacher/Teacher.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Teacher_navbar from './Teacher_navbar';
 import Header from '../../Header';
 import { Outlet } from 'react-router-dom';
 
 function Teacher() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className="flex min-h-screen bg-white">
-      <Teacher_navbar />
-      <div className="flex-1">
+      <Teacher_navbar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      <div
+        className={`transition-all duration-500 flex-1 ${
+          isSidebarOpen ? 'ml-64' : 'ml-24'
+        }`}
+      >
         <Header />
         <div className="p-4">
           <Outlet />
