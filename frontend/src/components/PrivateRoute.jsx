a5afbc4dcdfbeded9b289427e7185e6234748de2
Mathan-2007 <mathan007.2007@gmail.com>
2025-04-24 10:49:58 +0530

    hi

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,9 @@
+// components/PrivateRoute.jsx
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ children, role }) => {
+  const userRole = sessionStorage.getItem("role");
+  return userRole === role ? children : <Navigate to="/" />;
+};
+
+export default PrivateRoute;
