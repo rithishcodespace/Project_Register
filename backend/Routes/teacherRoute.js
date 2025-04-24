5b8f38ce958a4830cb56ed5a3e06f764ef00576d
rithishcodespace <rithishcodespace@gmail.com>
2025-04-24 09:08:07 +0530

    hi

--- backend/Routes/teacherRoute.js
+++ backend/Routes/teacherRoute.js
@@ -4,7 +4,7 @@ const createError = require("http-errors");
 const verifyAccessToken = require("../utils/verifyAccessToken");
 const db = require("../db");
 
-router.post("/teacher/addproject",verifyAccessToken,(req,res,next) => {
+router.post("/teacher/addproject",(req,res,next) => {
     try{
       const {project_id,project,cluster,description,phase_1_requirement,phase_1_deadline,phase_2_requirement,phase_2_deadline,phase_3_requirement,phase_3_deadline,phase_4_requirement,phase_4_deadline,phase_5_requirement,phase_5_deadline} = req.body;
       let sql = "INSERT INTO projects (project_id, project_name, cluster, description, phase_1_requirements, phase_1_deadline, phase_2_requirements, phase_2_deadline, phase_3_requirements, phase_3_deadline, phase_4_requirements, phase_4_deadline, phase_5_requirements, phase_5_deadline) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
