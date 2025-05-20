60ae7919f4312f4d25003be562b54a622ad99750
rithishcodespace <rithishcodespace@gmail.com>
2025-05-20 08:45:35 +0530

    pulling for madan

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mysql = require("mysql2");
 
-const pool1 = mysql.createPool({
+const pool = mysql.createPool({
     host:"localhost",
     user:"root",
     password:"Rithish@2006",
@@ -11,25 +11,25 @@ const pool1 = mysql.createPool({
     queueLimit:0
 })
 
-const pool = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"Mathan@2007",
-    database:"project_registor",
-    waitForConnections:true,
-    connectionLimit:10, 
-    queueLimit:0
-})
+// const pool = mysql.createPool({
+//     host:"localhost",
+//     user:"root",
+//     password:"Mathan@2007",
+//     database:"project_registor",
+//     waitForConnections:true,
+//     connectionLimit:10, 
+//     queueLimit:0
+// })
 
-const pool3 = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"Prakash@2007",
-    database:"project_registor",
-    waitForConnections:true,
-    connectionLimit:10, 
-    queueLimit:0
-})
+// const pool3 = mysql.createPool({
+//     host:"localhost",
+//     user:"root",
+//     password:"Prakash@2007",
+//     database:"project_registor",
+//     waitForConnections:true,
+//     connectionLimit:10, 
+//     queueLimit:0
+// })
 
 pool.getConnection((error,connection)=>{
     if(error) console.log("Error connecting to db",error.message);
